1fbacc1e7f6e981b1355ae69bf363793d15eb4da
EricSpencer00 <119877507+EricSpencer00@users.noreply.github.com>
2024-08-14 23:22:23 -0500

    barebones main.py

--- DexcomAPI/main.py
+++ DexcomAPI/main.py
@@ -11,8 +11,8 @@ import smtplib
 
 # Initialize variables
 dexcom = get_dexcom_connection()
-email_username, email_password = get_sender_email_credentials()
-receiver_email = get_receiver_email()
+# email_username, email_password = get_sender_email_credentials()
+# receiver_email = get_receiver_email()
 
 # Print the data to console
 print("\n\nmg/dL data:\n")
@@ -30,24 +30,24 @@ print("\n\n")
 generate_glucose_graph_mdgl(dexcom)
 
 message = MIMEMultipart()
-message["From"] = email_username
+# message["From"] = email_username
 message["To"] = receiver_email
 # message["Subject"] = "Glucose Level Alert"
 
 # Output to phone number
 message.attach(MIMEText(concise_message_mdgl(dexcom), 'plain'))
 
-try:
-    domain = email_username.split('@')[-1] # Support all email domains
-    smtp_server = f"smtp.{domain}"
-    smtp_port = 587
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(email_username, email_password)
-        server.sendmail(email_username, receiver_email, message.as_string())
-    print(f"Message sent successfully: {message}")
-except Exception as e:
-    print(f"error: {e}")
+# try:
+#     domain = email_username.split('@')[-1] # Support all email domains
+#     smtp_server = f"smtp.{domain}"
+#     smtp_port = 587
+#     with smtplib.SMTP(smtp_server, smtp_port) as server:
+#         server.starttls()
+#         server.login(email_username, email_password)
+#         server.sendmail(email_username, receiver_email, message.as_string())
+#     print(f"Message sent successfully: {message}")
+# except Exception as e:
+#     print(f"error: {e}")
 
 # Insert past 24 hours into database
 # db = get_database_connection()
